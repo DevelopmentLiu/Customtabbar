9d4385e398215aee59163a18c23cf409f9cfbe7f
Sunsline <hydom@HdtekiMac-mini.local>
2017-05-03 15:43:12 +0800

    Initial Commit

---
+++ Customtabbar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Customtabbar
+//
+//  Created by hydom on 2017/5/3.
+//  Copyright © 2017年 Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
